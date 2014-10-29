03f190cfcc6f2542112f40b2cbdaf65a9b7b201b
Mike Manger <mike.manger@lighthouseuk.net>
2014-10-29 15:12:44 +0000

    Simplify the library_term post type to not show front end pages.
    Also removes default args

--- admin/class-library-admin.php
+++ admin/class-library-admin.php
@@ -93,20 +93,11 @@ class Library_Admin {
 
 		$args = array(
 			'labels' => $labels,
-			'hierarchical' => false,
 			'supports' => array( 'title', 'editor', 'revisions' ),
-			'public' => true,
-			'show_ui' => true,
-			'show_in_menu' => true,
-			'menu_position' => 5,
+			'public' => false, // hide the front end UI
+			'show_ui' => true, // keep admin UI
 			'show_in_nav_menus' => true,
-			'publicly_queryable' => true,
-			'exclude_from_search' => false,
-			'has_archive' => true,
-			'query_var' => true,
-			'can_export' => true,
-			'rewrite' => true,
-			'capability_type' => 'post'
+			'menu_position' => 5,
 		);
 
 		register_post_type( 'library_term', $args );
