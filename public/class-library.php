a3276b07847a9f3caf95a62a374aef1bf32b2dd9
Mike Manger <ohmanger@gmail.com>
2016-10-06 15:34:20 +0100

    Use get_posts instead of WP_Query
    Reapplying 7473ed as it got mistakenly overwritten by 4928d8

--- public/class-library.php
+++ public/class-library.php
@@ -126,18 +126,13 @@ class Library {
 			'post_type' => 'library_term',
 		);
 
-		$query = new WP_Query( $args );
+		$posts = get_posts( $args );
 
 		$output = '';
 
-		if ( $query->have_posts() ) {
-			while ( $query->have_posts() ) {
-				$query->the_post();
-				$output = get_the_content();
-			}
+		foreach ( $posts as $post ) {
+			$output = $post->post_content;
 		}
-
-		wp_reset_postdata();
 
 		return $output;
 	}
